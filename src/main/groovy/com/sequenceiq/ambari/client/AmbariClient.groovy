b6eca858faeb4f121e564a1ca3eaadf06108380e
keyki <keyki.kk@gmail.com>
2014-05-19 14:17:43 +0200

    javadoc added

--- src/main/groovy/com/sequenceiq/ambari/client/AmbariClient.groovy
+++ src/main/groovy/com/sequenceiq/ambari/client/AmbariClient.groovy
@@ -23,6 +23,9 @@ import groovyx.net.http.RESTClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+/**
+ * Basic client to send requests to the Ambari server.
+ */
 class AmbariClient {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AmbariClient.class)
@@ -33,23 +36,50 @@ class AmbariClient {
   def slurper = new JsonSlurper();
   def clusterName
 
+  /**
+   * Connects to the ambari server.
+   *
+   * @param host host of the Ambari server; default value is localhost
+   * @param port port of the Ambari server; default value is 8080
+   * @param user username of the Ambari server; default is admin
+   * @param password password fom the Ambari server; default is admin
+   */
   AmbariClient(host = 'localhost', port = '8080', user = 'admin', password = 'admin') {
     ambari = new RESTClient("http://${host}:${port}/api/v1/" as String)
     ambari.headers['Authorization'] = 'Basic ' + "$user:$password".getBytes('iso-8859-1').encodeBase64()
     ambari.headers['X-Requested-By'] = 'ambari'
   }
 
+  /**
+   * Sets the debug variable. Used by printing the API calls for the Ambari Shell.
+   *
+   * @param enabled enable or disable
+   */
   def setDebugEnabled(boolean enabled) {
     debugEnabled = enabled;
   }
 
+  /**
+   * Returns the name of the cluster.
+   *
+   * @return the name of the cluster of null if no cluster yet
+   */
   def String getClusterName() {
     if (!clusterName) {
-      clusterName = getClusters().items[0]?.Clusters?.cluster_name
+      def clusters = getClusters();
+      if (clusters) {
+        clusterName = clusters.items[0]?.Clusters?.cluster_name
+      }
     }
     return clusterName
   }
 
+  /**
+   * Checks whether the blueprint exists or not.
+   *
+   * @param id id of the blueprint
+   * @return true if exists false otherwise
+   */
   def boolean doesBlueprintExists(String id) {
     def result = false
     try {
@@ -60,6 +90,12 @@ class AmbariClient {
     return result
   }
 
+  /**
+   * Returns a pre-formatted string of the blueprint.
+   *
+   * @param id id of the blueprint
+   * @return formatted string
+   */
   def String showBlueprint(String id) {
     def resp = getBlueprint(id)
     if (resp) {
@@ -73,44 +109,95 @@ class AmbariClient {
     return "Not found"
   }
 
+  /**
+   * Returns the host group - components mapping of the blueprint.
+   *
+   * @param id id of the blueprint
+   * @return Map where the key is the host group and the value is the list of components
+   */
   def Map<String, List<String>> getBlueprintMap(String id) {
     def result = getBlueprint(id).host_groups?.collectEntries { [(it.name): it.components.collect { it.name }] }
     result ? result : new HashMap()
   }
 
+  /**
+   * Returns a pre-formatted string of the blueprints.
+   *
+   * @return formatted blueprint list
+   */
   def String showBlueprints() {
     getBlueprints().items.collect { "${it.Blueprints.blueprint_name.padRight(PAD)} [${it.Blueprints.stack_name}:${it.Blueprints.stack_version}]" }.join("\n")
   }
 
+  /**
+   * Returns a Map containing the blueprint name - stack association.
+   *
+   * @return Map where the key is the blueprint's name value is the used stack
+   */
   def Map<String, String> getBlueprintsMap() {
     def result = getBlueprints().items?.collectEntries { [(it.Blueprints.blueprint_name): it.Blueprints.stack_name + ":" + it.Blueprints.stack_version] }
     result ? result : new HashMap()
   }
 
+  /**
+   * Returns a Map containing the blueprint's properties parsed from the Ambari response json.
+   *
+   * @return blueprint's properties as Map or empty Map
+   */
   def getBlueprints() {
     slurp("blueprints", "Blueprints")
   }
 
+  /**
+   * Returns the name of the host groups for a given blueprint.
+   *
+   * @param blueprint id of the blueprint
+   * @return host group list or empty list
+   */
   def List<String> getHostGroups(String blueprint) {
     def result = getBlueprint(blueprint)
     result ? result.host_groups.collect { it.name } : new ArrayList<String>()
   }
 
+  /**
+   * Returns a pre-formatted string of a blueprint used by the cluster.
+   *
+   * @return formatted string
+   */
   def String showClusterBlueprint() {
     ambari.get(path: "clusters/${getClusterName()}", query: ['format': "blueprint"]).data.text
   }
 
+  /**
+   * Adds a blueprint to the Ambari server.
+   *
+   * @param json blueprint as json
+   * @return true if the operation was successful false otherwise
+   */
   def boolean addBlueprint(String json) {
     if (json) {
       postBlueprint(json)
     }
   }
 
+  /**
+   * Adds 2 default blueprints.
+   *
+   * @return true if both added false otherwise
+   */
   def boolean addDefaultBlueprints() {
     return addBlueprint(getResourceContent("blueprints/multi-node-hdfs-yarn")) &&
       addBlueprint(getResourceContent("blueprints/single-node-hdfs-yarn"))
   }
 
+  /**
+   * Creates a cluster with the given blueprint and host group - host association.
+   *
+   * @param clusterName name of the cluster
+   * @param blueprintName blueprint id used to create this cluster
+   * @param hostGroups Map<String, List<String> key - host group, value - host list
+   * @return true if the creation was successful false otherwise
+   */
   def boolean createCluster(String clusterName, String blueprintName, Map hostGroups) {
     def result = true
     try {
@@ -122,6 +209,12 @@ class AmbariClient {
     return result
   }
 
+  /**
+   * Deletes the cluster.
+   *
+   * @param clusterName name of the cluster
+   * @return true if the operation was successful false otherwise
+   */
   def boolean deleteCluster(String clusterName) {
     def result = true
     try {
@@ -133,39 +226,88 @@ class AmbariClient {
     return result
   }
 
+  /**
+   * Returns a Map containing the cluster's properties parsed from the Ambari response json.
+   *
+   * @return cluster's properties as Map or empty Map
+   */
   def getClusters() {
     slurp("clusters", "Clusters")
   }
 
+  /**
+   * Returns a pre-formatted string of the clusters.
+   *
+   * @return pre-formatted cluster list
+   */
   def String showClusterList() {
     getClusters().items.collect { "[$it.Clusters.cluster_id] $it.Clusters.cluster_name:$it.Clusters.version" }.join("\n")
   }
 
+  /**
+   * Returns the task properties as Map.
+   *
+   * @param request request id; default is 1
+   * @return property Map or empty Map
+   */
   def getTasks(request = 1) {
     getAllResources("requests/$request", "tasks/Tasks")
   }
 
+  /**
+   * Returns a pre-formatted task list.
+   *
+   * @param request request id; default is 1
+   * @return pre-formatted task list
+   */
   def String showTaskList(request = 1) {
     getTasks(request)?.tasks.collect { "${it.Tasks.command_detail.padRight(PAD)} [${it.Tasks.status}]" }.join("\n")
   }
 
+  /**
+   * Returns a Map containing the task's command detail as key and the task's status as value.
+   *
+   * @param request request id; default is 1
+   * @return key task command detail; task value status
+   */
   def Map<String, String> getTaskMap(request = 1) {
     def result = getTasks(request).tasks?.collectEntries { [(it.Tasks.command_detail): it.Tasks.status] }
     result ? result : new HashMap()
   }
 
+  /**
+   * Returns the available hosts properties as a Map.
+   *
+   * @return Map containing the hosts properties
+   */
   def getHosts() {
     getAllResources("hosts", "Hosts")
   }
 
+  /**
+   * Returns a pre-formatted list of the hosts.
+   *
+   * @return pre-formatted string
+   */
   def String showHostList() {
     getHosts().items.collect { "$it.Hosts.host_name [$it.Hosts.host_status] $it.Hosts.ip $it.Hosts.os_type:$it.Hosts.os_arch" }.join("\n")
   }
 
+  /**
+   * Returns the service components properties as Map.
+   *
+   * @param service id of the service
+   * @return service component properties as Map
+   */
   def getServiceComponents(service) {
     getAllResources("services/$service/components", "ServiceComponentInfo")
   }
 
+  /**
+   * Returns a pre-formatted list of the service components.
+   *
+   * @return pre-formatted string
+   */
   def String showServiceComponents() {
     getServices().items.collect {
       def name = it.ServiceInfo.service_name
@@ -177,6 +319,12 @@ class AmbariClient {
     }.join("\n")
   }
 
+  /**
+   * Returns the service components properties as Map where the key is the service name and
+   * value is a component - state association.
+   *
+   * @return service name - [component name - status]
+   */
   def Map<String, Map<String, String>> getServiceComponentsMap() {
     def result = getServices().items?.collectEntries {
       def name = it.ServiceInfo.service_name
@@ -186,38 +334,77 @@ class AmbariClient {
     result ? result : new HashMap()
   }
 
+  /**
+   * Returns the services properties as Map parsed from Ambari response json.
+   *
+   * @return service properties as Map
+   */
   def getServices() {
     getAllResources("services", "ServiceInfo")
   }
 
+  /**
+   * Returns a pre-formatted service list.
+   *
+   * @return formatted string
+   */
   def String showServiceList() {
     getServices().items.collect { "${it.ServiceInfo.service_name.padRight(PAD)} [$it.ServiceInfo.state]" }.join("\n")
   }
 
+  /**
+   * Returns the services properties as Map where the key is the service's name and the values is the service's state.
+   *
+   * @return service name - service state association as Map
+   */
   def Map<String, String> getServicesMap() {
     def result = getServices().items?.collectEntries { [(it.ServiceInfo.service_name): it.ServiceInfo.state] }
     result ? result : new HashMap()
   }
 
+  /**
+   * Returns the properties of the host components as a Map parsed from the Ambari response json.
+   *
+   * @param host which host's components are requested
+   * @return component properties as Map
+   */
   def getHostComponents(host) {
     getAllResources("hosts/$host/host_components", "HostRoles")
   }
 
+  /**
+   * Returns a pre-formatted component list of a host.
+   *
+   * @param host which host's components are requested
+   * @return formatted string
+   */
   def String showHostComponentList(host) {
     getHostComponents(host).items.collect { "${it.HostRoles.component_name.padRight(PAD)} [$it.HostRoles.state]" }.join("\n")
   }
 
+  /**
+   * Returns the host's components as Map where the key is the component's name and values is its state.
+   *
+   * @param host which host's components are requested
+   * @return component name - state association
+   */
   def Map<String, String> getHostComponentsMap(host) {
     def result = getHostComponents(host).items?.collectEntries { [(it.HostRoles.component_name): it.HostRoles.state] }
     result ? result : new HashMap()
   }
 
-  def getAllResources(resourceName, fields) {
-    slurp("clusters/${getClusterName()}/$resourceName", "$fields/*")
-  }
-
+  /**
+   * Return the blueprint's properties as a Map.
+   *
+   * @param id id of the blueprint
+   * @return properties as Map
+   */
   def Map getBlueprint(id) {
     slurp("blueprints/$id", "host_groups,Blueprints")
+  }
+
+  private def getAllResources(resourceName, fields) {
+    slurp("clusters/${getClusterName()}/$resourceName", "$fields/*")
   }
 
   /**
