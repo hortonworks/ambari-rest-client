aa417e9f85f2002d41d05aaf58016ef74e5d3a0e
keyki <keyki.kk@gmail.com>
2014-05-28 10:47:28 +0200

    get install progress

--- src/main/groovy/com/sequenceiq/ambari/client/AmbariClient.groovy
+++ src/main/groovy/com/sequenceiq/ambari/client/AmbariClient.groovy
@@ -573,6 +573,7 @@ class AmbariClient {
   }
 
   private def slurp(path, fields = "") {
+
     def fieldsMap = fields ? ['fields': fields] : [:]
     def Map resourceReqMap = getResourceRequestMap(path, fieldsMap)
     def result = getResource(resourceReqMap)
