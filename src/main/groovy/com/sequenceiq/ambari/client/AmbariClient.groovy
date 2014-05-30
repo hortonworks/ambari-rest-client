fa9f06e4aa80a3464012e60e8ec2c61ce4660ce2
keyki <keyki.kk@gmail.com>
2014-05-30 15:58:27 +0200

    missing import added

--- src/main/groovy/com/sequenceiq/ambari/client/AmbariClient.groovy
+++ src/main/groovy/com/sequenceiq/ambari/client/AmbariClient.groovy
@@ -24,6 +24,9 @@ import groovyx.net.http.HttpResponseException
 import groovyx.net.http.RESTClient
 import org.apache.http.NoHttpResponseException
 import org.apache.http.client.ClientProtocolException
+import java.net.ConnectException
+import java.net.NoRouteToHostException
+import java.net.UnknownHostException
 
 /**
  * Basic client to send requests to the Ambari server.
